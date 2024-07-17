2d4e0aa152e6f2b3c48478ff8c51badf9960c204
Soeren Balke <sb@fokuspokus-media.de>
2024-07-17 09:14:20 +0200

    add capacitor android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ninja.samyblake.homeapp',
+  appName: 'home-app',
+  webDir: 'dist/browser',
+};
+
+export default config;
